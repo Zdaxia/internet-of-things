c774b2a894474c6690ae41eba572357f9507db75
Zdaxia <zouziqu@gmail.com>
2014-10-14 21:55:55 -0400

    add Xively into js

--- mid-term/js/main.js
+++ mid-term/js/main.js
@@ -1,5 +1,40 @@
-(function(){
+(function() {
 
-	console.log('hi');
+	xively.setKey("Db0azaSMVCmuajrtVk9IzytbqNVreX29ou8HbeHSZ3L0D0Ox");
+
+	var feedID = 196923667, // Feed ID  
+		datastreamID1 = "meal"; // Datastream ID  
+	var fishIhadToday = 0;
+	// selector = "#myelement"; // Your element on the page  
+
+	xively.datastream.get(feedID, datastreamID1, function(datastream) {
+
+		//first round for current data
+
+		console.log('----current_value ' + datastream["current_value"]);
+		fishIhadToday = datastream["current_value"];
+		$('#fishNumber').text(fishIhadToday);
+		$(".fishContiner").empty();
+		for (var i = 0; i < fishIhadToday; i++) {
+			$('.fishContiner').append('<div class="fish"></div>');
+		};
+
+		// Getting realtime!   
+		// The function associated with the subscribe method will be executed   
+		// every time there is an update to the datastream  
+		xively.datastream.subscribe(feedID, datastreamID1, function(event, datastream_updated) {
+			// Display the current value from the updated datastream  
+			// $(selector).html(datastream_updated["current_value"]);
+			console.log(datastream_updated["current_value"]);
+			fishIhadToday = datastream_updated["current_value"];
+			$('#fishNumber').text(fishIhadToday);
+			$(".fishContiner").empty();
+			for (var i = 0; i < fishIhadToday; i++) {
+				$('.fishContiner').append('<div class="fish"></div>');
+			};
+		});
+
+	});
+
 
 }());
